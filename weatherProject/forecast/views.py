f8195e39acccdc013d05f73678af41915c54a055
NPraveenLiyanage <nipunaliyange44@gmail.com>
2025-06-17 11:20:40 +0530

    add dependencies and neccery files

---
+++ weatherProject/forecast/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def weather_view(request):
+    return HttpResponse('<h1>Weather Prediction App</h1>')
